2f0e5cf38a779a5f45f4e8c2e8ddcb6dce233d87
Jason Howlin <jhowlin@me.com>
2019-09-18 05:46:43 -0400

    Fix typo

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@
 import PackageDescription
 
 let package = Package(
-  name: "WikipediaKit"
-)
-
-let package = Package(
     name: "WikipediaKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
