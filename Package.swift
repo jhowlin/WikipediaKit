09a50ae898ed3d0874ff260e7810e0d0d4e78718
Jason Howlin <jhowlin@me.com>
2019-09-18 05:45:54 -0400

    Target

--- Package.swift
+++ Package.swift
@@ -5,3 +5,21 @@ import PackageDescription
 let package = Package(
   name: "WikipediaKit"
 )
+
+let package = Package(
+    name: "WikipediaKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "WikipediaKit",
+            targets: ["WikipediaKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "WikipediaKit",
+            dependencies: []),
+    ]
+)
+
